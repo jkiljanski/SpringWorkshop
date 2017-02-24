450b27756b4d10ba96eb97fdd950a3a7898c9304
Jacek Kiljański <EXT.Jacek.Kiljanski@dsv.com>
2017-02-24 16:28:29 +0100

    refactored Spring exercise 1 - reusing logic

--- beans/src/main/java/workshop/spring/exercises/ex1/EvenNumberGiver.java
+++ beans/src/main/java/workshop/spring/exercises/ex1/EvenNumberGiver.java
@@ -1,11 +1,7 @@
 package workshop.spring.exercises.ex1;
 
-public class EvenNumberGiver implements SequentialNumberGiver {
-    private int i;
-    @Override
-    public int nextNumber() {
-        i++;
-        i++;
-        return i;
+public class EvenNumberGiver extends NextNumberGiver {
+    public EvenNumberGiver() {
+        super(2);
     }
 }
