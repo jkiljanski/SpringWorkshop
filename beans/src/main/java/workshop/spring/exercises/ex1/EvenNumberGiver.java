7d5c6f3f86952c05ff50b1cc680418f27797d1f6
Jacek Kiljański <EXT.Jacek.Kiljanski@dsv.com>
2017-02-24 16:32:54 +0100

    refactored Spring exercise 1 - transforming inheritance to combining

--- beans/src/main/java/workshop/spring/exercises/ex1/EvenNumberGiver.java
+++ beans/src/main/java/workshop/spring/exercises/ex1/EvenNumberGiver.java
@@ -1,7 +1,15 @@
 package workshop.spring.exercises.ex1;
 
-public class EvenNumberGiver extends NextNumberGiver {
-    public EvenNumberGiver() {
-        super(2);
+public class EvenNumberGiver implements SequentialNumberGiver{
+    private final NextNumberGiver nextNumberGiver;
+
+    public EvenNumberGiver(NextNumberGiver nextNumberGiver) {
+        this.nextNumberGiver = nextNumberGiver;
+    }
+
+    @Override
+    public int nextNumber() {
+        nextNumberGiver.nextNumber();
+        return nextNumberGiver.nextNumber();
     }
 }
--- beans/src/main/java/workshop/spring/exercises/ex1/NextNumberGiver.java
+++ beans/src/main/java/workshop/spring/exercises/ex1/NextNumberGiver.java
@@ -2,16 +2,9 @@ package workshop.spring.exercises.ex1;
 
 public class NextNumberGiver implements SequentialNumberGiver{
     private int i=0;
-    private final int multiplier;
-
-    public NextNumberGiver(int multiplier) {
-        this.multiplier = multiplier;
-    }
-
 
     @Override
     public int nextNumber() {
-        i+=multiplier;
-        return i;
+        return ++i;
     }
 }
