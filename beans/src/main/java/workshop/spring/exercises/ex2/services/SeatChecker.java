df2b5f362b8282888c0b1a0201f633e3628a740b
Jacek Kiljański <EXT.Jacek.Kiljanski@dsv.com>
2017-02-27 11:04:13 +0100

    Refactor ex2 Spring - logging interface

--- beans/src/main/java/workshop/spring/exercises/ex2/services/SeatChecker.java
+++ beans/src/main/java/workshop/spring/exercises/ex2/services/SeatChecker.java
@@ -7,12 +7,12 @@ import workshop.spring.exercises.ex2.domain.Seat;
 import workshop.spring.exercises.ex2.domain.SeatBelt;
 
 @Service
-public class SeatChecker implements BusPartChecker<Seat>{
+public class SeatChecker extends LoggingBusPartChecker <Seat> implements BusPartChecker<Seat>{
 
     @Autowired
     BusPartChecker<SeatBelt> seatBeltChecker;
 
-    public void check(Seat seat, SafetyReport safetyReport) {
+    public void innerCheck(Seat seat, SafetyReport safetyReport) {
         if (seat.howManyHoles>0){
             safetyReport.addThingToFix(seat);
         }
