ac3c60f7255441c6d1e67f98f6bd8104f6d47000
Jacek Kiljański <jkiljanski@pzu.pl>
2021-04-29 12:37:48 +0200

    better car description

--- javaIntroduction/src/main/java/workshop/oop/drive/cars/CarSeller.java
+++ javaIntroduction/src/main/java/workshop/oop/drive/cars/CarSeller.java
@@ -8,17 +8,22 @@ import java.util.stream.Collectors;
 public class CarSeller {
 
     public String describe(Car car) {
-        return "Car is a " + car.describeColor() + " " + car.describeType();
+        String carWithOptionalPosition = "Car";
+        return createDescription(carWithOptionalPosition, car);
+    }
+
+    private String createDescription(String carWithOptionalPosition, Car car) {
+        return carWithOptionalPosition + " is a " + car.describeColor() + " " + car.describeType();
     }
 
     public String describe(Car... cars) {
         return Stream.of(cars)
                 .zipWithIndex()
-                .map(carIntegerTuple2 -> {
-                    Car car = carIntegerTuple2._1;
-                    Integer position = carIntegerTuple2._2 + 1;
+                .map(carWithIndex -> {
+                    Car car = carWithIndex._1;
+                    Integer position = carWithIndex._2 + 1;
 
-                    return "Car#" + position + " is a " + car.describeColor() + " " + car.describeType();
+                    return createDescription("Car#" + position, car);
                 })
                 .collect(Collectors.joining(", "));
 
